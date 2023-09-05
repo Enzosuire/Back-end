06a06062166b6fd333aaaaf9c2b54cc7fdc04ee1
Enzosuire <enzosuire@yahoo.com>
2023-09-05 17:02:27 +0200

    exercice javascript

---
+++ Exercice Javascript/test.Js -damier/Script/script.js
@@ -0,0 +1,12 @@
+console.log ("ca marche");
+
+
+for (let colonne = 1; colonne<=8 ; colonne++) {
+   console.log ("colonne");
+
+   for (let colonne = 1; colonne<=8 ; colonne++) {
+      console.log ("colonne");
+
+}
+}
+
