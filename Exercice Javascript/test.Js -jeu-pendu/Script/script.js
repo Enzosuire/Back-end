4755797f3df712f312582d27fa4f4b4613266e98
Enzosuire <enzosuire@yahoo.com>
2023-09-10 11:20:12 +0200

    exercice jeu du pendu

--- Exercice Javascript/test.Js -jeu-pendu/Script/script.js
+++ Exercice Javascript/test.Js -jeu-pendu/Script/script.js
@@ -3,36 +3,45 @@ console.log("script chargé");
 const UNELettre = document.createElement("div");
 
 
-
-
-
-
-
-const UNELettrearray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const UNELettrearray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 console.log(UNELettre);
 
-for (let i = 0; i<UNELettrearray.length; i++) {
-   console.log ("button");
+for (let i = 0; i < UNELettrearray.length; i++) {
+   console.log("button");
 
-   let malettrebutton= document.createElement("button");
+   let malettrebutton = document.createElement("button");
    malettrebutton.innerText = UNELettrearray[i];
    UNELettre.appendChild(malettrebutton);
+
+   malettrebutton.classList.add("casecache");
+   malettrebutton.addEventListener("click",(eventdetail)=> {
+      const lettreCliquee= eventdetail.target.innerText;
+      UNELettre.appendChild(lettreCliquee);
+   });
    
-   malettrebutton.classList.add("casecache")
 
 }
 
+
 document.body.appendChild(UNELettre);
+function afficherLettre(lettre) {
+   const lettreDiv = document.createElement("div");
+   lettreDiv.innerText = lettre;
+  afficherLettre(lettreCliquee);
 
-document.querySelectorAll("button").forEach((i) => 
-UNELettre.addEventListener("click", (eventdetail) =>{
-   eventdetail.target.disabled = true;
-   
+}
+
+document.querySelectorAll("button").forEach((i) =>
+   UNELettre.addEventListener("click", (eventdetail) => {
+      eventdetail.target.disabled = true;
 
 
-})
+
+   })
 );
 
 let buttonElt = document.createElement("button");
 UNELettre.appendChild(buttonElt);
 buttonElt.innerText = "ENVOYER";
+
+
