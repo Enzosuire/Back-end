38be9451e6268c696574e1ff45ad2bbfbadb42dd
Enzosuire <enzosuire@yahoo.com>
2023-09-14 17:00:45 +0200

    pokemon

--- Exercice Javascript/test.Js -Pokemon-selecteur/Script/fetcth.js
+++ Exercice Javascript/test.Js -Pokemon-selecteur/Script/fetcth.js
@@ -1,46 +1,53 @@
 console.log("script chargé");
 
-function getValues(event) {
-  event.preventDefault(); 
+let element = document.querySelector("select");
+const selectElement = document.querySelector("select");
 
-let element= document.querySelector('select');
+function NomPokemon() {
+  const Api = `https://pokebuildapi.fr/api/v1/pokemon/`;
 
-for (let i = 0; i < array.length; i++) {
-    
+  //liaison api pokemon//
 
-      
+  fetch(Api)
+    .then((response) => response.json())
+    .then((data) => {
+      // Parcourez les données pour créer des options dans le select
+      data.forEach((pokemon) => {
+        const option = document.createElement("option");
+        option.value = pokemon.name; // Assurez-vous que "name" correspond au nom du Pokémon dans les données
+        option.textContent = pokemon.name;
+        selectElement.appendChild(option);
+      });
+    });
 }
 
+NomPokemon();
+function getValues(event) {
+  event.preventDefault();
 
-//liaison api pokemon//
+  let element = document.querySelector("select");
+  const selectedPokemon = selectElement.value;
 
-fetch(`https://pokebuildapi.fr/api/v1/pokemon/${FormPokemon}`)
-      .then(response => response.json())
-      .then(data => {
+  const Api2 = `https://pokebuildapi.fr/api/v1/pokemon/${selectedPokemon}`;
 
-      
+  //liaison api pokemon//
 
-      console.log(`Détails de ${FormPokemon}:`);
-      console.log(`Nom: ${data.name}`);
-      console.log(`Image: ${data.image}`);
-      console.log(`Element: ${data.apiTypes[0].name}`);
-      console.log(`Evolue: ${data.apiEvolutions[0].name}`);
-
-
-      document.querySelector("h2").textContent = `Voici les informations de ${FormPokemon}`;
+  fetch(Api2)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(
+        "h2"
+      ).textContent = `Voici les informations de ${selectedPokemon}`;
       document.querySelector("img").setAttribute("src", data.image);
       let arrayP = document.querySelectorAll(".resultat>p");
-      arrayP[0].textContent = `Element: ${data.apiTypes[0].name}`;
-      arrayP[1].textContent = `Evolue: ${data.apiEvolutions[0].name}`;
-
-      
-      })
+      arrayP[0].textContent = `Élement: ${data.apiTypes[0].name}`;
      
-    
+
+      document.querySelector("#details").style.visibility = "visible";
+   
+    arrayP[1].textContent = `Évolution: ${data.apiEvolutions[0].name}`;
+});
 }
 
-      let monFormulaire = document.forms["FormPokemon"];
-      console.log(monFormulaire);
-      monFormulaire.addEventListener("submit", getValues); // Remarquez que l'on n'envoie rien et que l'on appelle la fonction sans les parenthèses
-      
-
+const FElement = document.querySelector("form");
+FElement.addEventListener("submit", getValues);
