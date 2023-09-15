14b42f3894aa68c5dbb62c44e4a962d966611240
Enzosuire <enzosuire@yahoo.com>
2023-09-15 11:59:37 +0200

    exercice pokemon

--- Exercice Javascript/test.Js -Pokemon-selecteur/Script/fetcth.js
+++ Exercice Javascript/test.Js -Pokemon-selecteur/Script/fetcth.js
@@ -1,7 +1,7 @@
 console.log("script chargé");
 
-let element = document.querySelector("select");
 const selectElement = document.querySelector("select");
+const detailsButton = document.querySelector("#details");
 
 function NomPokemon() {
   const Api = `https://pokebuildapi.fr/api/v1/pokemon/`;
@@ -22,10 +22,10 @@ function NomPokemon() {
 }
 
 NomPokemon();
+
 function getValues(event) {
   event.preventDefault();
 
-  let element = document.querySelector("select");
   const selectedPokemon = selectElement.value;
 
   const Api2 = `https://pokebuildapi.fr/api/v1/pokemon/${selectedPokemon}`;
@@ -40,13 +40,78 @@ function getValues(event) {
       ).textContent = `Voici les informations de ${selectedPokemon}`;
       document.querySelector("img").setAttribute("src", data.image);
       let arrayP = document.querySelectorAll(".resultat>p");
-      arrayP[0].textContent = `Élement: ${data.apiTypes[0].name}`;
-     
+      let arrayTypes = [];
+      data.apiTypes.forEach((type) => {
+        arrayTypes.push(type.name);
+      });
+      let stringTypes = arrayTypes.join("/");
+      console.log(stringTypes);
+
+      let arrayEvolutions = [];
+      data.apiEvolutions.forEach((evolutions) => {
+        arrayEvolutions.push(evolutions.name);
+      });
+      let stringEvolutions = arrayEvolutions.join("/");
+      console.log(stringEvolutions);
+
+      arrayP[0].textContent = `Élement: ${stringTypes}`;
+      arrayP[1].textContent = `Évolution: ${stringEvolutions}`;
 
       document.querySelector("#details").style.visibility = "visible";
-   
-    arrayP[1].textContent = `Évolution: ${data.apiEvolutions[0].name}`;
-});
+
+
+      detailsButton.addEventListener("click", () => {
+        
+        const statsApi = `https://pokebuildapi.fr/api/v1/pokemon/${selectedPokemon}/`;
+        
+        fetch(statsApi)
+          .then((response) => response.json())
+          .then((statsData) => {
+            // Traitez les données des statistiques ici
+
+            //..........................
+            const main= document.getElementById("main")
+
+            const option3 = document.createElement("div");
+            option3.textContent =("Les statistique :");
+            main.appendChild(option3);
+           
+            const option2 = document.createElement("div");
+            option2.textContent =("Points de vie : " +statsData.stats.HP);
+            main.appendChild(option2);
+
+            const option4 = document.createElement("div");
+            option4.textContent =("attaque : " +statsData.stats.attack);
+            main.appendChild(option4);
+
+            const option5 = document.createElement("div");
+            option5.textContent =("Deffense : " +statsData.stats.defense);
+            main.appendChild(option5);
+
+            const option6 = document.createElement("div");
+            option6.textContent =("Attaque spécial : " +statsData.stats.special_attack);
+            main.appendChild(option6);
+
+            const option7 = document.createElement("div");
+            option7.textContent =("Deffense spécial : " +statsData.stats.special_defense);
+            main.appendChild(option7);
+
+            const option8 = document.createElement("div");
+            option8.textContent =("Vitesse : " +statsData.stats.speed);
+            main.appendChild(option8);
+
+
+           
+           
+            console.log(statsData);
+
+            //............................
+            console.log(statsData.stats.HP);
+            // Vous pouvez afficher les statistiques comme vous le souhaitez
+           
+          });
+      });
+    });
 }
 
 const FElement = document.querySelector("form");
